2b4ac5c849e2cea52e2da570e2007f014fdc9bfa
gloryofme <gloryofme@163.com>
2016-03-25 00:47:06 +0800

    done

---
+++ src/fkal/ExchangerTest.java
@@ -0,0 +1,52 @@
+package fkal;
+
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExchangerTest {
+
+	private static final Exchanger<String>exgr=new Exchanger<>();
+	
+	private static ExecutorService threadPool=Executors.newFixedThreadPool(2);
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			threadPool.execute(new Runnable(){
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					String A="from a string";
+					try {
+						exgr.exchange(A);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			});
+			threadPool.execute(new Runnable(){
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					String B="from b string";
+					try {
+						String A=exgr.exchange(B);
+						System.out.println("A和B录入的数据是否一致："+A.equals(B));
+						System.out.println("A录入的数据是："+A);
+						System.out.println("B录入的数据是："+B);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			});
+			threadPool.shutdown();
+	}
+
+}
---
+++ src/fkal/SemaphoreTest.java
@@ -0,0 +1,35 @@
+package fkal;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+	
+	private static final int THREAD_COUNT=30;
+	
+	private static ExecutorService threadPool=Executors.newFixedThreadPool(THREAD_COUNT);
+	
+	private static Semaphore s=new Semaphore(10);
+	
+	public static void main(String []args){
+		for(int i=0;i<THREAD_COUNT;i++){
+			threadPool.execute(new Runnable(){
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					try {
+						s.acquire();
+						System.out.println("save data ");
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}finally{
+						s.release();
+					}
+				}
+			});
+		}
+		threadPool.shutdown();
+	}
+}
