44af84a291cd202e47530954449542f61b293751
gloryofme <gloryofme@163.com>
2016-03-25 00:08:54 +0800

    done

--- src/fkal/BankWaterService.java
+++ src/fkal/BankWaterService.java
@@ -1,5 +1,7 @@
 package fkal;
 
+import java.util.Map.Entry;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executor;
@@ -15,7 +17,7 @@ public class BankWaterService implements Runnable {
 	/**
 	 * 创建4个屏障，处理完之后执行当前类的run方法
 	 */
-	private CyclicBarrier c=new CyclicBarrier(4);
+	private CyclicBarrier c=new CyclicBarrier(4,this);
 	/**
 	 * 假设只有4个sheet，所以只启动4个线程
 	 */
@@ -25,10 +27,37 @@ public class BankWaterService implements Runnable {
 	 */
 	private ConcurrentHashMap<String,Integer>sheetBankWaterCount=new ConcurrentHashMap<String,Integer>();
 	
+	private void count(){
+		for(int i=0;i<4;i++){
+			executor.execute(new Runnable(){
+
+				@Override
+				public void run() {
+					// 计算当前sheet的银行流水数据
+					sheetBankWaterCount.put(Thread.currentThread().getName(),1);
+					try {
+						c.await();
+					} catch (InterruptedException | BrokenBarrierException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		int result=0;
+		for(Entry<String,Integer>sheet:sheetBankWaterCount.entrySet()){
+			result+=sheet.getValue();
+		}
+		//输出结果
+		sheetBankWaterCount.put("result",result);
+		System.out.println("count="+result);
 	}
-
+	public static void main(String []args){
+		BankWaterService bankWaterCount=new BankWaterService();
+		bankWaterCount.count();
+	}
 }
